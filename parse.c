0ab756ce0e94c3fa0c91f9f14a857ff987637cf5
jendralhxr <jendral.hxr@gmail.com>
2017-07-03 10:31:38 +0900

    simple parser/row select from accelerometer's csv

---
+++ parse.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SELECTROW 0
+FILE *text;
+double val[3][9];
+
+int main(int argc, char **argv){
+	text= fopen(argv[1], "r");
+	int i, j, n;
+	for (n=0; n<atoi(argv[2]); n++){
+		// 0	-0.04	-0.04	-0.04	-0.04	-0.03	-0.04	-0.04	-0.04
+		fscanf(text,"%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",\
+			&(val[i][0]), &(val[i][1]), &(val[i][2]), &(val[i][3]), &(val[i][4]),\
+			&(val[i][5]), &(val[i][6]), &(val[i][7]), &(val[i][8])); 
+		i++;
+	if(i==3){
+		i=0;
+		// average;
+	for (j=0; j<9; j++){
+		val[SELECTROW][j] = (val[0][j] + val[1][j] + val[2][j]) /3;
+		}
+		// select row (1 out of 3)
+		j= SELECTROW;
+		printf("%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\t%lf\n",\
+			(val[j][0]), (val[j][1]), (val[j][2]), (val[j][3]), (val[j][4]),\
+			(val[j][5]), (val[j][6]), (val[j][7]), (val[j][8]));
+		}
+	}
+	return(atoi(argv[2]));
+	}

2424e238851c05ce46b574493cf3f747fd8fbcb5
jendralhxr <jendral.hxr@gmail.com>
2017-09-27 11:10:40 +0900

    reuse the cvblob function

--- moment.cpp
+++ moment.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iostream>
 #include <iomanip>
 #include <fstream>
 #include <opencv2/opencv.hpp>
@@ -34,8 +35,8 @@ unsigned int calculate_historam(){
 		}
 	}
 
-unsigned int calculate_width(int thresholdval){
-	double moment_x_temp, moment_y_temp;
+unsigned int calculate_width(int thresholdval, int sep){
+	double moment_x_temp, moment_y_temp, mass_temp, moment_x, moment_y;
 	int x_min=image_width, x_max=0, y_min=image_height, y_max=0;
 	
 	offset = image_width*image_height - 1;
@@ -43,17 +44,19 @@ unsigned int calculate_width(int thresholdval){
 	if (image.data[offset] > thresholdval) {
 		moment_x_temp = (offset % image_width);
 		moment_y_temp = (offset / image_width);
-		if (moment_y_temp>y_max) y_max= moment_y_temp;
-		if (moment_y_temp<y_min) y_min= moment_y_temp;
-		if (moment_x_temp>x_max) x_max= moment_x_temp;
-		if (moment_x_temp<x_min) x_min= moment_x_temp;
+		if ((offset % image_width) < sep){
+			if (moment_y_temp>y_max) y_max= moment_y_temp;
+			if (moment_y_temp<y_min) y_min= moment_y_temp;
+			if (moment_x_temp>x_max) x_max= moment_x_temp;
+			if (moment_x_temp<x_min) x_min= moment_x_temp;
+			mass_temp++;
+			moment_x += moment_x_temp;
+			moment_y += moment_y_temp;
+			}
 		}
 	offset--;
 	if (offset>image_width) goto width;
-	
-	printf("marker: %d %d %d %d %d %d\n",x_max, x_min, y_max, y_min ,x_max-x_min, y_max-y_min);
-		
-	
+	printf("marker: %lf %lf %d %d\n",moment_x/mass_temp, moment_y/mass_temp, x_max-x_min, y_max-y_min);
 	}
 
 
@@ -128,9 +131,8 @@ rower:
 	return(0);
 	} 
 
-
-unsigned int calculate_moment_gray() {
-	Mat moment_map = Mat(image_height, image_width, CV_8UC1);
+unsigned int calculate_moment_gray(int sep) {
+	//Mat moment_map = Mat(image_height, image_width, CV_8UC1);
 	double moment_x_temp, moment_y_temp, mass_temp;
 	int object_counts = 0, offset = 0;
 	
@@ -138,15 +140,72 @@ unsigned int calculate_moment_gray() {
 	offset = image_width*image_height - 1;
 restofimage:
 	if (image.data[offset] > THRESHOLD_GRAY) {
-		mass_temp++;
-		moment_x_temp += (offset % image_width);
-		moment_y_temp += (offset / image_width);
+		if ((offset % image_width) > sep){
+			mass_temp++;
+			moment_x_temp += (offset % image_width);
+			moment_y_temp += (offset / image_width);
+			}
 		}
 	offset--;
 	if (offset>image_width) goto restofimage;
 	
 	cout << setprecision(8) << moment_x_temp/mass_temp << ',' << moment_y_temp/mass_temp << ';';
 	return(mass_temp);
+}
+
+int cvblob(){
+	float moment_x[MAX_OBJECTS], moment_y[MAX_OBJECTS], mass[MAX_OBJECTS];
+	float moment_x_temp, moment_y_temp, mass_temp;
+	// Set up the detector with default parameters.
+	//SimpleBlobDetector detector;
+	SimpleBlobDetector::Params params;
+	params.filterByColor = false;
+	params.filterByCircularity = false;
+	params.filterByConvexity = true;
+	params.filterByInertia = true;
+	//params.maxThreshold = 220;
+	//params.minThreshold = 100;
+	SimpleBlobDetector detector(params);
+
+	// Detect blobs.
+	std::vector<KeyPoint> keypoints;
+	//Mat invert; bitwise_not(image, invert); detector.detect(invert, keypoints);
+	detector.detect(image, keypoints);
+
+	int n=0; 
+	for (std::vector<KeyPoint>::iterator it = keypoints.begin(); it != keypoints.end(); ++it){
+		//cout << it->pt.x  << ';' << it->pt.y << endl;
+		moment_x[n]= it->pt.x;
+		moment_y[n]= it->pt.y;
+		mass[n]= it->size;
+		n++;
+		} //  std::cout << ' ' << *it;	
+	
+	// little sorting
+	for (int i=n-1; i>=0; i--){
+		for (int j=i-1; j>=0; j--){
+			if (moment_x[i] > moment_x[j]){
+				moment_x_temp= moment_x[i];
+				moment_y_temp= moment_y[i];
+				mass_temp= mass[i];
+				moment_x[i]= moment_x[j];
+				moment_y[i]= moment_y[j];
+				mass[i]= mass[j];
+				moment_x[j]= moment_x_temp;
+				moment_y[j]= moment_y_temp;
+				mass[j]= mass_temp;
+				}
+			}
+		}
+	
+	// the log	
+	for (int i=n-1; i>=0; i--){
+		//logfile << moment_x[i] << ',' << moment_y[i] << ',' << mass[i] <<';';
+		cout << moment_x[i] 	<< ',' << moment_y[i] << ';';
+		}
+	//logfile << n;	
+	logfile << endl;
+	return(n);
 }
 
 // parse image sequence	
@@ -158,19 +217,22 @@ int main(int argc, char **argv){
 	image_width = image_input.cols;
 	//printf("%s %d %d: ",filename, image_width, image_height);
 	int mrgins[MARKER_COUNT]={2048};
-	int separator= 282;
-	int row1[9]={282, 464, 621, 804, 1002, 1208, 1417, 1647, 1936};
-	int row2[9]={373, 536, 669, 827, 1013, 1195, 1423, 1660, 1941};
+	int separator= 1570;
+	int row1[9]={151, 282, 423, 600, 775, 973, 1168, 1386, 1686};
+	int row2[9]={336, 518, 679, 841, 1029, 1253, 1479, 1736, 2021};
 	
 	if (image_height && image_width) {
-		//cvtColor(image_input, temp, CV_BGR2GRAY);
-		//transpose(temp, image); // in case of rotated camera
-		cvtColor(image_input, image, CV_BGR2GRAY);
+		cvtColor(image_input, temp, CV_BGR2GRAY);
+		transpose(temp, image); // in case of rotated camera
+		flip(image, image, 0);
+		//cvtColor(image_input, image, CV_BGR2GRAY);
 		//imshow("disp",image); waitKey(0);
 		if (!strcmp(argv[1],"h")) calculate_historam();
+		else if (!strcmp(argv[1],"g")) calculate_moment_gray(atoi(argv[3]));
 		else if (!strcmp(argv[1],"c")) calculate_niner(mrgins);
 		else if (!strcmp(argv[1],"d")) calculate_rower(separator, row1, row2);
-		else if (!strcmp(argv[1],"w")) calculate_width(atoi(argv[3]));
+		else if (!strcmp(argv[1],"w")) calculate_width(atoi(argv[3]), atoi(argv[4]));
+		else if (!strcmp(argv[1],"v")) cvblob();
 		else {
 			cout << "wrong argument for argv[1]" << endl; 
 			return(0);

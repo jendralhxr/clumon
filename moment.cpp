dc8338693ef63007ed2d53f66d6fd6899dc34edb
jendralhxr <jendral.hxr@gmail.com>
2017-11-20 13:53:00 +0900

    average of cumulative histogram

--- moment.cpp
+++ moment.cpp
@@ -7,7 +7,7 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <math.h>
 
-#define THRESHOLD_GRAY 25
+#define THRESHOLD_GRAY 50
 #define MAX_OBJECTS 1024
 #define MARKER_ROWS 9
 #define MARKER_COUNT 18
@@ -22,21 +22,27 @@ int image_height, image_width;
 int framenum;
 unsigned int offset;
 
-unsigned int calculate_historam(){
+unsigned int calculate_histogram(int threshold){
 	double max_intensity=0;
-	double hist_value[256]; // 8-bit depth
+	double hist_value[256]; // 8-bit depth image
+	double cumulative_value, cumulative_count;
 	int i;
 	
 	offset = image_width*image_height - 1;
 	histo:
 	hist_value[image.data[offset]]+=1;
+	if (image.data[offset]>threshold){
+		cumulative_count++;
+		cumulative_value+=image.data[offset];
+		}
 	if (image.data[offset]>max_intensity) max_intensity= image.data[offset];
 	offset--;
 	if (offset>0) goto histo;
 	for (i=0; i<256; i++){
 		printf("hist %d: %.0f\n",i,hist_value[i]);
 		}
-	printf("max: %.0f\n", max_intensity);
+	cumulative_value /= cumulative_count;	
+	printf("%s max: %.0f; avg: %.0f\n", filename, max_intensity, cumulative_value);
 	}
 
 unsigned int calculate_width(int thresholdval, int sep){
@@ -58,9 +64,31 @@ unsigned int calculate_width(int thresholdval, int sep){
 			moment_y += moment_y_temp;
 			}
 		}
+		
 	offset--;
 	if (offset>image_width) goto width;
-	printf("marker: %lf %lf %d %d\n",moment_x/mass_temp, moment_y/mass_temp, x_max-x_min, y_max-y_min);
+	printf("%s: %.0lf %.2lf %.2lf %d %d \n",filename, mass_temp, moment_x/mass_temp, moment_y/mass_temp, x_max-x_min, y_max-y_min);
+	
+	/*
+	// minimum width/height
+	x_max=image_width, x_min=0, y_max=image_height, y_min=0;
+	offset = image_width*image_height - 1;
+	width:
+	if (image.data[offset] > thresholdval) {
+		moment_x_temp = (offset % image_width);
+		moment_y_temp = (offset / image_width);
+		if ((offset % image_width) < sep){
+			if (moment_y_temp<y_max) y_max= moment_y_temp;
+			if (moment_y_temp>y_min) y_min= moment_y_temp;
+			if (moment_x_temp>x_max) x_max= moment_x_temp;
+			if (moment_x_temp<x_min) x_min= moment_x_temp;
+			mass_temp++;
+			moment_x += moment_x_temp;
+			moment_y += moment_y_temp;
+			}
+		}
+	//printf("min: %.0lf %lf %lf %d %d \n",mass_temp, moment_x/mass_temp, moment_y/mass_temp, x_max-x_min, y_max-y_min);
+	*/
 	}
 
 unsigned int calculate_niner(int *margin){
@@ -91,7 +119,7 @@ niner:
 	return(0);
 	} 
 
-unsigned int calculate_rower(int separator, int *margin1, int *margin2){
+unsigned int calculate_rower(int separator, int *margin1, int *margin2, int threshold){
 	double moment_x[MARKER_COUNT], moment_y[MARKER_COUNT], mass[MARKER_COUNT];
 	double moment_x_temp, moment_y_temp, mass_temp;
 	
@@ -100,7 +128,7 @@ unsigned int calculate_rower(int separator, int *margin1, int *margin2){
 
 	offset = image_width*image_height - 1;
 rower:
-	if (image.data[offset] > THRESHOLD_GRAY) {
+	if (image.data[offset] > threshold) {
 		moment_x_temp = (offset % image_width);
 		moment_y_temp = (offset / image_width);
 		if (moment_x_temp>separator){ // first row markers
@@ -215,27 +243,27 @@ int cvblob(){
 int main(int argc, char **argv){
 	//logfile.open("/me/log.txt");
 	sprintf(filename, "%s", argv[2]);
-	image_input = imread(argv[2], 1);
+	image_input = imread(filename, 1);
 	image_height = image_input.rows;
 	image_width = image_input.cols;
 	//printf("%s %d %d: ",filename, image_width, image_height);
 	int mrgins[MARKER_COUNT]={2048};
-	int separator= 370;
-	int row2[9]={1500, 1234, 1060, 872, 688, 500, 378, 228, 10};
-	int row1[9]={1616, 1372, 1137, 930, 744, 576, 420, 270, 50};
+	int separator= 378;
+	int row2[9]={1500, 1234, 1060, 872, 688, 500, 378, 228, 10}; // left side
+	int row1[9]={1539, 1278, 1077, 906, 729, 564, 405, 255, 20}; // right side
 	
 	if (image_height && image_width) {
 		// in case of rotated camera, 3 lines
 		cvtColor(image_input, temp, CV_BGR2GRAY);
-		transpose(temp, image); 
+		transpose(temp, image);  // two lines, rotate 90 deg clockwise
 		flip(image, image, 1);
 		// or it is already in grayscale
 		//cvtColor(image_input, image, CV_BGR2GRAY);
-		//imshow("disp",image); waitKey(0);
-		if (!strcmp(argv[1],"h")) calculate_historam();
-		else if (!strcmp(argv[1],"g")) calculate_moment_gray(atoi(argv[3]));
-		else if (!strcmp(argv[1],"c")) calculate_niner(mrgins);
-		else if (!strcmp(argv[1],"d")) calculate_rower(separator, row1, row2);
+		// imshow("disp",image); waitKey(0);
+		if (!strcmp(argv[1],"h")) calculate_histogram(atoi(argv[3]));
+		//else if (!strcmp(argv[1],"g")) calculate_moment_gray(atoi(argv[3]));
+		//else if (!strcmp(argv[1],"c")) calculate_niner(mrgins);
+		else if (!strcmp(argv[1],"d")) calculate_rower(separator, row1, row2, atoi(argv[3]));
 		else if (!strcmp(argv[1],"w")) calculate_width(atoi(argv[3]), atoi(argv[4]));
 		else if (!strcmp(argv[1],"v")) cvblob();
 		else {

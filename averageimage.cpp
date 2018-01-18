299704ace2d8ddad47f2bbc214f0985e5a4cb8f5
jendralhxr <jendral.hxr@gmail.com>
2018-01-18 16:10:54 +0900

    average of several consecutive images

---
+++ averageimage.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <math.h>
+
+#define THRESHOLD_GRAY 15
+#define MAX_OBJECTS 1024
+#define MARKER_ROWS 9
+#define MARKER_COUNT 18
+#define MARKER_COUNT_REF 20
+using namespace std;
+using namespace cv;
+
+ofstream logfile;
+char filename[256];
+
+Mat image, temp;
+int image_height, image_width;
+int framenum;
+unsigned int offset;
+double **block;
+
+using namespace cv;
+
+// parse image sequence	
+int main(int argc, char **argv){
+	//logfile.open("/me/log.txt");
+	int imagecount;
+	imagecount= atoi(argv[3]) - atoi(argv[2]);
+	if (imagecount<0) return(4);
+	else printf("processing average of %d images\n", imagecount);
+	
+	sprintf(filename, "%s%05d.tif", argv[1], atoi(argv[2]));
+	image = imread(filename, 1);
+	temp = imread(filename, 1);
+	image_height = image.rows;
+	image_width = image.cols;
+	printf("size: %d %d\n",image_width, image_height);
+	
+	block =  (double**) malloc(sizeof(double *) * image_height);
+	for (int j=0; j<image_height; j++){
+		block[j] = (double*) malloc(sizeof(double)* image_width);
+		}
+		
+	int i, j;
+	for (int num=atoi(argv[2]); num<atoi(argv[3]); num++){
+		sprintf(filename, "%s%05d.tif", argv[1], num);
+		printf("%s\n",filename);
+		image = imread(filename, 1);
+		for (j=0; j<image_height; j++){
+			for (i=0; i<image_width; i++){
+				block[j][i]= image.data[image_width*j + i] / imagecount; 
+				}
+			}
+		}
+	
+	for (j=0; j<image_height; j++){
+		for (i=0; i<image_width; i++){
+			temp.data[image_width*j + i]= (char) block[j][i]; 
+			}
+		}
+	imwrite("average.tif", temp);	
+		
+	
+	return(0);
+}
+
+

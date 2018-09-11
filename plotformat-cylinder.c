6bab5a6748f836da80e8c423bce4858d45eb8156
yoen <jendral.hxr@gmail.com>
2018-09-11 10:28:21 +0900

    fgets is your friend
    fgets works better than outright fscanf. I've never really figured out why so

--- plotformat-cylinder.c
+++ plotformat-cylinder.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MULTIPLIER_RAD 10
 
 FILE *input;
 FILE *output;
 FILE *output2;
 int linecount;
 char filename[128], filename2[128];
+char linebuffer[256];
 
 int parse9singleside(){
 	float point[10];
 	int t;
 	for (t=0; t<linecount; t++){
-		printf("line %d\n",t);
-		fscanf(input,"%f %f %f %f %f %f %f %f %f %f\n",\
+		fgets(linebuffer, 256, input);
+//		printf("%d: %s",t, linebuffer);
+		sscanf(linebuffer,"%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n",\
 		&point[0],\
 		&point[1], &point[2], &point[3],\
 		&point[4], &point[5], &point[6],\
@@ -21,15 +24,16 @@ int parse9singleside(){
 		fprintf(output,"# t=%d\n",t);
 		// 0	72	122	172	222	272	322	372	414
 		// gnuplot requires theta-z-radius
-		fprintf(output,"%f %f %f\n", point[0], -7.0, point[1]);
-		fprintf(output,"%f %f %f\n", point[0], 65.0, point[2]);
-		fprintf(output,"%f %f %f\n", point[0], 115.0, point[3]);
-		fprintf(output,"%f %f %f\n", point[0], 165.0, point[4]);
-		fprintf(output,"%f %f %f\n", point[0], 215.0, point[5]);
-		fprintf(output,"%f %f %f\n", point[0], 265.0, point[6]);
-		fprintf(output,"%f %f %f\n", point[0], 315.0, point[7]);
-		fprintf(output,"%f %f %f\n", point[0], 365.0, point[8]);
-		fprintf(output,"%f %f %f\n", point[0], 407.0, point[9]);
+		// theta is in radian, z is in cm, radius is in mm (before  multiplier) 
+		fprintf(output,"%f %f %f\n", point[0], -7.0, point[1]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 65.0, point[2]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 115.0, point[3]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 165.0, point[4]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 215.0, point[5]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 265.0, point[6]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 315.0, point[7]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 365.0, point[8]*MULTIPLIER_RAD);
+		fprintf(output,"%f %f %f\n", point[0], 407.0, point[9]*MULTIPLIER_RAD);
 		fprintf(output,"\n");
 		}
 		return(t);
@@ -38,9 +42,17 @@ int parse9singleside(){
 int main(int argc, char **argv){
 	input= fopen(argv[1], "r");
 	printf("opening intput file %s\n",argv[1]);
+	if (input==NULL){
+		perror("error opening input file");
+		return(1);
+		}
 	output= fopen(argv[2], "w");
 	linecount= atoi(argv[3]);
-	printf("opening output file %s for %d lines\n",argv[2], linecount);
+	if (output==NULL){
+		perror("error opening input file");
+		return(1);
+		}
+	else printf("opening output file %s for %d lines\n",argv[2], linecount);
 	
 	parse9singleside();
 	fclose(output);
